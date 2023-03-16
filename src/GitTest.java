24fe9e7d6685f47ecef3f70fb8b2b8b765f0dee5
Salory <1720287895@qq.com>
2023-03-16 17:03:22 +0800

    hot-fix first commit.

---
+++ src/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * 项目名: gitTest
+ * 文件名: GitTest
+ * 创建者: LZS
+ * 创建时间:2023/3/16 17:02
+ * 描述:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
