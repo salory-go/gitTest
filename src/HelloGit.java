3e21e251039e1ceedc81f8709c90cdeaaa09c98f
Salory <1720287895@qq.com>
2023-03-16 16:33:16 +0800

    hot-fix first commit.

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -7,7 +7,6 @@
  **/
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git.");
-        System.out.println("Hello Git2.");
+        System.out.println("Hello hot-fix");
     }
 }
