c0df6a6e4334ff8bb25d9d482718059f9c246363
Salory <1720287895@qq.com>
2023-03-16 16:20:22 +0800

    Idea first commit.

---
+++ src/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * 项目名: gitTest
+ * 文件名: HelloGit
+ * 创建者: LZS
+ * 创建时间:2023/3/16 10:39
+ * 描述:
+ **/
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git.");
+    }
+}
