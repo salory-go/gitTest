7b7ee4205421b40b67a3fed9686834b8cb315079
Salory <1720287895@qq.com>
2023-03-25 15:05:44 +0800

    hot-fix second commit.

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,3 +1,6 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * 项目名: gitTest
  * 文件名: GitTest
@@ -6,8 +9,12 @@
  * 描述:
  **/
 public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("hello github");
-        System.out.println("111111");
+    public static void main(String[] args) throws UnknownHostException {
+//        System.out.println("hello github");
+//        System.out.println("111111");
+        InetAddress localHost = InetAddress.getLocalHost();
+        System.out.println(localHost);
+        System.out.println(localHost.getHostName());
+        System.out.println(localHost.getHostAddress());
     }
 }
